9e18ace3239a960607f1baff5cbbfab9b51aaff0
Scott Greenough <scott.greenough@gmail.com>
2016-08-30 15:34:41 -0400

    Examples page update

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,23 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = () => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>welcome to the examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out</p>
+      <ol>
+        <li>
+          <Link to='/?location=Raleigh,nc'>
+            Raleigh, NC
+          </Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>
+            Rio, Brazil
+          </Link>
+        </li>
+      </ol>
     </div>
   )
 };
