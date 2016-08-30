b423ecaa071f8afec8e07ec824e82119eb599fa9
Scott Greenough <scott.greenough@gmail.com>
2016-08-30 10:53:39 -0400

    Changed About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,8 +2,10 @@ var React = require('react');
 
 var About = () => {
   return (
-    <h3>About</h3>
-    <p>Welcome to the about page!</p>
+    <div>
+      <h3>About</h3>
+      <p>Welcome to the about page!</p>      
+    </div>
   )
 }
 module.exports = About;
