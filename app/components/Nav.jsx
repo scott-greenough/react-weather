969fc6dac4d121570835ced769aa15ed853c6cb5
System Administrator <root@Scott-Greenoughs-iMac.local>
2016-09-01 17:13:31 -0400

    Fixed search box on nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,8 +4,16 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch: function (e) {
       e.preventDefault();
-      alert('Not yet wired up!');
+
+      var location = this.refs.search.value;
+      var encodedLocation = encodeURIComponent(location);
+
+      if (location.length > 0){
+        this.refs.search.value = '';
+        window.location.hash = '#/?location=' + encodedLocation;
+      }
   },
+
   render: function () {
     return (
       <div className="top-bar">
@@ -27,7 +35,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder="search weather by city"/>
+                <input type="search" ref="search" placeholder="search weather by city"/>
               </li>
               <li>
                 <input type="submit" className="button" value="Get Weather"/>
